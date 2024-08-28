656d3fdf392cce7a7aaf80bebd6d042630a0cbac
arshad1992shaikh <143215843+arshad1992shaikh@users.noreply.github.com>
2024-08-28 10:19:19 +0530

    initial commit

---
+++ src/main/java/com/timeFirst/course/report/Api/integration/CourseReportApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.timeFirst.course.report.Api.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseReportApiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseReportApiIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timeFirst/course/report/Api/integration/CourseReportApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeFirst.course.report.Api.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseReportApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
